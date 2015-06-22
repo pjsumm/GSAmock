469e2b46f0ab234dc1f58a2f5da8d36c64e52df0
Bryan Allred <bryan.allred@gmail.com>
2015-06-22 10:59:57 -0400

    Task #53: Added quick partial templates and stubbed controllers

---
+++ TrueTandem.GSAmock/TrueTandem.GSAmock/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrueTandem.GSAmock.Controllers
+{
+    /// <summary>
+    /// The home controller.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
