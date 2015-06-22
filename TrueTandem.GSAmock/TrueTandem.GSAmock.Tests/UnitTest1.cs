79e6a75605fbb3c30819b9d621b00181b0514f36
Paul Summers <paul.summers@truetandem.com>
2015-06-22 10:04:27 -0400

    Added new Unit test to confirm CI build unit test case run on commit

---
+++ TrueTandem.GSAmock/TrueTandem.GSAmock.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrueTandem.GSAmock.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CIBuildTestMethod1()
+        {
+            // Assert
+            Assert.AreEqual("True", "True");
+        }
+    }
+}
